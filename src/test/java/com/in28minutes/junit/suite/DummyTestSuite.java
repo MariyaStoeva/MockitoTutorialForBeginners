ef707c41e16bc84a7a4f6db9824d853da6eeeda7
MariyaStoeva <mariya.adrianova.stoeva@gmail.com>
2017-08-02 10:01:55 +0300

    add junit tests for string helper

--- src/test/java/com/in28minutes/junit/suite/DummyTestSuite.java
+++ src/test/java/com/in28minutes/junit/suite/DummyTestSuite.java
@@ -1,15 +1,44 @@
 package com.in28minutes.junit.suite;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.mockito.Mock;
 
 import com.in28minutes.junit.helper.ArraysTest;
+import com.in28minutes.junit.helper.StringHelper;
 import com.in28minutes.junit.helper.StringHelperTest;
 
-@RunWith(Suite.class)
+//@RunWith(Suite.class)
 @SuiteClasses({ArraysTest.class,StringHelperTest.class})
 public class DummyTestSuite {
+
+	public StringHelper sHelper = new StringHelper();
+	public void Coverage() {
+		testTruncateAInFirst2Positions();
+		testAreFirstAndLastTwoCharactersTheSame();
+	}
+	@Test
+	public void testTruncateAInFirst2Positions() {
+		
+		
+		String testString1 = sHelper.truncateAInFirst2Positions("qwerty");
+		String testString2 = "A erty";
+		testString1.equals(testString2);
+	}
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame() {
+		Boolean areTheSameTrue1 = sHelper.areFirstAndLastTwoCharactersTheSame("AbcgfA");
+		Boolean areTheSameTrue2 = sHelper.areFirstAndLastTwoCharactersTheSame("SbcgfS");
+		Boolean areTheSameFalse = sHelper.areFirstAndLastTwoCharactersTheSame("SbcgfA");
+		
+		
+		areTheSameTrue1.equals(areTheSameTrue2);
+		areTheSameTrue1.equals(!areTheSameFalse);
+		
+	}
+	
 
 }
