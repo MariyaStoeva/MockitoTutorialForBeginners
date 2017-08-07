74d9dbe0a954aef232c38a875fcd43562c0cac8f
MariyaStoeva <mariya.adrianova.stoeva@gmail.com>
2017-08-07 11:33:05 +0300

    commit

---
+++ src/main/java/com/in28minutes/main/Application.java
@@ -0,0 +1,22 @@
+package com.in28minutes.main;
+
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+
+public class Application {
+
+	public static void main(String[] args) throws LDAPException {
+		//finalizeClient();
+
+	}
+//
+//	public static synchronized void finalizeClient() throws LDAPException
+//	{
+//		  LDAPConnection conn =new LDAPConnection();
+//		  conn.connect("localhost", 10389);
+//		 
+//		 
+//		  
+//	  conn.close();
+//	}
+}
---
+++ src/test/java/com/in28minutes/ldap/EmbeddedLdapTest.java
@@ -0,0 +1,53 @@
+package com.in28minutes.ldap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.zapodot.junit.ldap.EmbeddedLdapRule;
+import org.zapodot.junit.ldap.EmbeddedLdapRuleBuilder;
+
+import com.unboundid.ldap.sdk.LDAPInterface;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
+import com.unboundid.ldap.sdk.persist.LDAPField;
+
+/**
+ * Test using objectclass: org.zapodot:embedded-ldap-junit
+ */
+public class EmbeddedLdapTest {
+	public static final String DOMAIN_DSN = "dc=example,dc=com";
+	
+//	String ldifFiles = "resources\\users-import.ldif";
+	
+	
+	@Rule
+	public EmbeddedLdapRule embeddedLdapRule = EmbeddedLdapRuleBuilder.newInstance().usingDomainDsn(DOMAIN_DSN)
+			.importingLdifs("users-import.ldif").build();
+//
+//	@Test
+//	public void shouldFindAllPersons() throws Exception {
+//		final LDAPInterface ldapConnection = embeddedLdapRule.ldapConnection();
+//		final SearchResult searchResult = ldapConnection.search(DOMAIN_DSN, SearchScope.SUB, "(objectClass=person)");
+//		assertThat(0, equalTo(searchResult.getEntryCount()));
+//		List<SearchResultEntry> searchEntries = searchResult.getSearchEntries();
+//		assertThat(searchEntries.get(0).getAttribute("cn").getValue(), equalTo("Micha Kops"));
+//		assertThat(searchEntries.get(1).getAttribute("cn").getValue(), equalTo("Santa Claus"));
+//		assertThat(searchEntries.get(2).getAttribute("cn").getValue(), equalTo("John Steinbeck"));
+//	}
+
+	@Test
+	public void shouldFindExactPerson() throws Exception {
+		final LDAPInterface ldapConnection = embeddedLdapRule.ldapConnection();
+		final SearchResult searchResult = ldapConnection.search("cn=Santa Claus,ou=Users,dc=example,dc=com",
+				SearchScope.BASE, "(objectClass=person)");
+		assertThat(1, equalTo(searchResult.getEntryCount()));
+		assertThat(searchResult.getSearchEntries().get(0).getAttribute("cn").getValue(), equalTo("Santa Claus"));
+	}
+	
+
+}
